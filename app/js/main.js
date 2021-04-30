87d0dd497e143581f45f2d13c916391caf594e03
gedonik <gedonik@bk.ru>
2021-04-30 18:32:42 +0300

    added popular gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,13 @@
 $(function () {
+  $(".popular__btn").on("click", function (event) {
+    event.preventDefault();
+  });
 
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false
   });
+
+  var mixer = mixitup('.popular__content');
 
 });
